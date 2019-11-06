b6f5ba5b412d7746456986a617fd45a4a6eb83e5
sourcefx <legolas.mw12@gmail.com>
2019-11-06 14:37:27 +0100

    create DataManagement Interface

---
+++ Rent-A-PC/Rent-A-PC/DataManagement/DataManagementSQL.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rent_A_PC.Model;
+
+namespace Rent_A_PC.DataManagement
+{
+    class DataManagementSQL : IDataManagement
+    {
+        public bool Delete(Pc pc)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(Customer custormers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Insert(List<Pc> pcs)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Insert(List<Customer> customers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Pc pc)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Customer customers)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Rent-A-PC/Rent-A-PC/IDataManagement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rent_A_PC.Model;
+
+namespace Rent_A_PC
+{
+    interface IDataManagement
+    {
+        //Pc
+        //bool Save();
+        bool Insert(List<Pc> pcs);
+        bool Update(Pc pc);
+        bool Delete(Pc pc);
+
+        //Customer
+        //bool Save();
+        bool Insert(List<Customer> customers);
+        bool Update(Customer customers);
+        bool Delete(Customer custormers);
+    }
+}
