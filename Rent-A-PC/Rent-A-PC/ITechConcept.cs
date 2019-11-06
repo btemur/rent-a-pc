18dcae22e63eb61573333bbae35e15be2df76e9b
sourcefx <legolas.mw12@gmail.com>
2019-11-06 14:51:11 +0100

    insert files

---
+++ Rent-A-PC/Rent-A-PC/ITechConcept.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rent_A_PC.Model;
+
+namespace Rent_A_PC
+{
+    interface ITechConcept
+    {
+        List<Pc> SortPc();
+        List<Customer> SortCustomer();
+    }
+}
---
+++ Rent-A-PC/Rent-A-PC/TechConcepts/TechConceotAsc.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rent_A_PC.Model;
+
+namespace Rent_A_PC.TechConcepts
+{
+    class TechConceotAsc : ITechConcept
+    {
+        public List<Customer> SortCustomer()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Pc> SortPc()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
