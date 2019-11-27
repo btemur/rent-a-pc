4a91e2b5b31e9a2042bb008409c593c7210762cb
sourcefx <legolas.mw12@gmail.com>
2019-11-27 11:01:29 +0100

    Arbeits ergebnis

--- Rent-A-PC/Rent-A-PC/Form1.Designer.cs
+++ Rent-A-PC/Rent-A-PC/Form1.Designer.cs
@@ -36,16 +36,16 @@
             this.rbFilterAll = new System.Windows.Forms.RadioButton();
             this.rbFilterLeased = new System.Windows.Forms.RadioButton();
             this.rbFilterAvailable = new System.Windows.Forms.RadioButton();
-            this.buttonNewName = new System.Windows.Forms.Button();
+            this.buttonAddNewItem = new System.Windows.Forms.Button();
             this.buttonUpdate = new System.Windows.Forms.Button();
             this.buttonDelete = new System.Windows.Forms.Button();
             this.buttonExit = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBoxPCName = new System.Windows.Forms.TextBox();
             this.lablePcName = new System.Windows.Forms.Label();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.cb_LeaseState = new System.Windows.Forms.CheckBox();
             this.gb_Output = new System.Windows.Forms.GroupBox();
-            this.gb_Items = new System.Windows.Forms.GroupBox();
             this.label_Filter = new System.Windows.Forms.Label();
+            this.gb_Items = new System.Windows.Forms.GroupBox();
             this.gb_Output.SuspendLayout();
             this.gb_Items.SuspendLayout();
             this.SuspendLayout();
@@ -93,6 +93,7 @@
             // 
             this.richTextBox1.Location = new System.Drawing.Point(6, 59);
             this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
             this.richTextBox1.Size = new System.Drawing.Size(436, 261);
             this.richTextBox1.TabIndex = 5;
             this.richTextBox1.Text = "";
@@ -130,15 +131,15 @@
             this.rbFilterAvailable.Text = "available";
             this.rbFilterAvailable.UseVisualStyleBackColor = true;
             // 
-            // buttonNewName
+            // buttonAddNewItem
             // 
-            this.buttonNewName.Enabled = false;
-            this.buttonNewName.Location = new System.Drawing.Point(6, 383);
-            this.buttonNewName.Name = "buttonNewName";
-            this.buttonNewName.Size = new System.Drawing.Size(75, 23);
-            this.buttonNewName.TabIndex = 9;
-            this.buttonNewName.Text = "Add new";
-            this.buttonNewName.UseVisualStyleBackColor = true;
+            this.buttonAddNewItem.Enabled = false;
+            this.buttonAddNewItem.Location = new System.Drawing.Point(6, 383);
+            this.buttonAddNewItem.Name = "buttonAddNewItem";
+            this.buttonAddNewItem.Size = new System.Drawing.Size(75, 23);
+            this.buttonAddNewItem.TabIndex = 9;
+            this.buttonAddNewItem.Text = "Add new";
+            this.buttonAddNewItem.UseVisualStyleBackColor = true;
             // 
             // buttonUpdate
             // 
@@ -168,14 +169,15 @@
             this.buttonExit.TabIndex = 12;
             this.buttonExit.Text = "Exit";
             this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
             // 
-            // textBox1
+            // textBoxPCName
             // 
-            this.textBox1.Location = new System.Drawing.Point(83, 327);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(145, 20);
-            this.textBox1.TabIndex = 13;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBoxPCName.Location = new System.Drawing.Point(83, 327);
+            this.textBoxPCName.Name = "textBoxPCName";
+            this.textBoxPCName.Size = new System.Drawing.Size(145, 20);
+            this.textBoxPCName.TabIndex = 13;
+            this.textBoxPCName.TextChanged += new System.EventHandler(this.TextBoxPCName_TextChanged);
             // 
             // lablePcName
             // 
@@ -186,27 +188,27 @@
             this.lablePcName.TabIndex = 14;
             this.lablePcName.Text = "PC name:";
             // 
-            // checkBox1
+            // cb_LeaseState
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.checkBox1.Enabled = false;
-            this.checkBox1.Location = new System.Drawing.Point(234, 330);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(60, 17);
-            this.checkBox1.TabIndex = 16;
-            this.checkBox1.Text = "leased:";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.cb_LeaseState.AutoSize = true;
+            this.cb_LeaseState.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.cb_LeaseState.Enabled = false;
+            this.cb_LeaseState.Location = new System.Drawing.Point(234, 330);
+            this.cb_LeaseState.Name = "cb_LeaseState";
+            this.cb_LeaseState.Size = new System.Drawing.Size(60, 17);
+            this.cb_LeaseState.TabIndex = 16;
+            this.cb_LeaseState.Text = "leased:";
+            this.cb_LeaseState.UseVisualStyleBackColor = true;
             // 
             // gb_Output
             // 
             this.gb_Output.Controls.Add(this.label_Filter);
             this.gb_Output.Controls.Add(this.rbFilterAvailable);
             this.gb_Output.Controls.Add(this.buttonExit);
-            this.gb_Output.Controls.Add(this.checkBox1);
+            this.gb_Output.Controls.Add(this.cb_LeaseState);
             this.gb_Output.Controls.Add(this.rbFilterAll);
             this.gb_Output.Controls.Add(this.lablePcName);
-            this.gb_Output.Controls.Add(this.textBox1);
+            this.gb_Output.Controls.Add(this.textBoxPCName);
             this.gb_Output.Controls.Add(this.rbFilterLeased);
             this.gb_Output.Controls.Add(this.lableInfo);
             this.gb_Output.Controls.Add(this.buttonDelete);
@@ -219,19 +221,6 @@
             this.gb_Output.TabStop = false;
             this.gb_Output.Text = "Output";
             // 
-            // gb_Items
-            // 
-            this.gb_Items.Controls.Add(this.rbPc);
-            this.gb_Items.Controls.Add(this.listBox);
-            this.gb_Items.Controls.Add(this.buttonNewName);
-            this.gb_Items.Controls.Add(this.rbCustomer);
-            this.gb_Items.Location = new System.Drawing.Point(12, 12);
-            this.gb_Items.Name = "gb_Items";
-            this.gb_Items.Size = new System.Drawing.Size(131, 412);
-            this.gb_Items.TabIndex = 18;
-            this.gb_Items.TabStop = false;
-            this.gb_Items.Text = "Items";
-            // 
             // label_Filter
             // 
             this.label_Filter.AutoSize = true;
@@ -240,6 +229,19 @@
             this.label_Filter.Size = new System.Drawing.Size(32, 13);
             this.label_Filter.TabIndex = 17;
             this.label_Filter.Text = "Filter:";
+            // 
+            // gb_Items
+            // 
+            this.gb_Items.Controls.Add(this.rbPc);
+            this.gb_Items.Controls.Add(this.listBox);
+            this.gb_Items.Controls.Add(this.buttonAddNewItem);
+            this.gb_Items.Controls.Add(this.rbCustomer);
+            this.gb_Items.Location = new System.Drawing.Point(12, 12);
+            this.gb_Items.Name = "gb_Items";
+            this.gb_Items.Size = new System.Drawing.Size(131, 412);
+            this.gb_Items.TabIndex = 18;
+            this.gb_Items.TabStop = false;
+            this.gb_Items.Text = "Items";
             // 
             // Form1
             // 
@@ -268,13 +270,13 @@
         private System.Windows.Forms.RadioButton rbFilterAll;
         private System.Windows.Forms.RadioButton rbFilterLeased;
         private System.Windows.Forms.RadioButton rbFilterAvailable;
-        private System.Windows.Forms.Button buttonNewName;
+        private System.Windows.Forms.Button buttonAddNewItem;
         private System.Windows.Forms.Button buttonUpdate;
         private System.Windows.Forms.Button buttonDelete;
         private System.Windows.Forms.Button buttonExit;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBoxPCName;
         private System.Windows.Forms.Label lablePcName;
-        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox cb_LeaseState;
         private System.Windows.Forms.GroupBox gb_Output;
         private System.Windows.Forms.GroupBox gb_Items;
         private System.Windows.Forms.Label label_Filter;
